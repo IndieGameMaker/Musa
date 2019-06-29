96a5ae19097b2b4746d2fac991f2f9b643eb5019
IndieGameMaker <myleje@gmail.com>
2019-06-29 16:57:06 +0900

    Initial commit

---
+++ Assets/02.Scripts/TouchMgr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMgr : MonoBehaviour
+{
+    private Camera cam;
+    private Ray ray;
+    private RaycastHit hit;
+
+    void Start()
+    {
+        cam = Camera.main;        
+    }
+
+    void Update()
+    {
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.green);
+
+        if (Input.GetMouseButtonDown(0)
+            && Physics.Raycast(ray, out hit, 100.0f, 1<<8))
+            {
+                Destroy(hit.collider.gameObject);
+            }        
+    }
+}

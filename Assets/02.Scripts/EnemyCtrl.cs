ba8efacae8e099d7fa4de37682af7d8b3afe347f
IndieGameMaker <myleje@gmail.com>
2019-07-06 13:54:06 +0900

    .

--- Assets/02.Scripts/EnemyCtrl.cs
+++ Assets/02.Scripts/EnemyCtrl.cs
@@ -11,6 +11,7 @@ public class EnemyCtrl : MonoBehaviour
     private Animator anim;
 
     public Transform[] points;
+    public int nextIdx = 1;
 
     void Start()
     {
@@ -20,11 +21,19 @@ public class EnemyCtrl : MonoBehaviour
         anim        = GetComponent<Animator>();
 
         points = GameObject.Find("WayPointGroup").GetComponentsInChildren<Transform>();
+
+        nv.SetDestination(points[nextIdx].position);
+        nv.isStopped = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (nv.velocity.sqrMagnitude >= 0.2f * 0.2f //속도가 0.2 클때 --> 이동 중 일때
+           && nv.remainingDistance <= 0.2f)         //목적지까지 남은 거리가 0.2(20cm) 이하일 경우
+        {
+            nextIdx = (++nextIdx >= points.Length) ? 1 : nextIdx;
+            nv.SetDestination(points[nextIdx].position);
+            nv.isStopped = false;
+        }
     }
 }

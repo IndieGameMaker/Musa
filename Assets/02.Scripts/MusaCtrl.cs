798b18021f1698b24ae33800674d0ab965ef0d63
IndieGameMaker <myleje@gmail.com>
2019-07-06 10:23:45 +0900

    .

--- Assets/02.Scripts/MusaCtrl.cs
+++ Assets/02.Scripts/MusaCtrl.cs
@@ -30,6 +30,15 @@ public class MusaCtrl : MonoBehaviour
             ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position); //첫 번째 손가락의 좌표
         }
 #endif
-
+        //바닥만 검출하는 레이캐스트를 캐스팅
+        if (Physics.Raycast(ray, out hit, 20.0F, 1<<10))
+        {
+            //이동해야할 지점까지의 벡터를 계산
+            Vector3 dir = hit.point - tr.position;
+            //벡터의 각도(쿼터니언 타입으로 산출)
+            Quaternion rot = Quaternion.LookRotation(dir);
+            //주인공 캐릭터의 점진적으로 회전
+            tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * damping);
+        }
     }
 }

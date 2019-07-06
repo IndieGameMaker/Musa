9ec1be6be3ec0c0aee02efb6205a773b07f041ca
IndieGameMaker <myleje@gmail.com>
2019-07-06 13:42:45 +0900

    .

--- Assets/02.Scripts/EnemyCtrl.cs
+++ Assets/02.Scripts/EnemyCtrl.cs
@@ -10,12 +10,16 @@ public class EnemyCtrl : MonoBehaviour
     private NavMeshAgent nv;
     private Animator anim;
 
+    public Transform[] points;
+
     void Start()
     {
         tr          = GetComponent<Transform>();
         playerTr    = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         nv          = GetComponent<NavMeshAgent>();
         anim        = GetComponent<Animator>();
+
+        points = GameObject.Find("WayPointGroup").GetComponentsInChildren<Transform>();
     }
 
     // Update is called once per frame

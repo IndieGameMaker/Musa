5e354c10d48374595bdc159977f124c1d0757bee
IndieGameMaker <myleje@gmail.com>
2019-07-06 09:16:14 +0900

    .

--- Assets/02.Scripts/TouchMgr.cs
+++ Assets/02.Scripts/TouchMgr.cs
@@ -29,10 +29,11 @@ public class TouchMgr : MonoBehaviour
     //폭발효과 발생
     void Explosion(Vector3 point)
     {
+        //순간적으로 컬라이더를 오버랩해서 대상을 추출
         Collider[] colls = Physics.OverlapSphere(point, 10.0f, 1<<9);
         foreach(var coll in colls)
         {
-            
+            coll.GetComponent<Rigidbody>().AddExplosionForce(1500.0f, point, 10.0f, 1800.0f);
         }
     }
 

94c6c58651233b0a001056d11af6f1da129e9376
IndieGameMaker <myleje@gmail.com>
2019-07-06 15:37:45 +0900

    .

---
+++ Assets/02.Scripts/Drag.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IDragHandler
+{
+    private Transform tr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        tr.position = Input.mousePosition;
+    }
+
+}

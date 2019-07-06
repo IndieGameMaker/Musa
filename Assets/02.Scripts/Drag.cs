e75d7d3d168a3689410581d39dc7f0a2a20744ff
IndieGameMaker <myleje@gmail.com>
2019-07-06 15:41:10 +0900

    .

--- Assets/02.Scripts/Drag.cs
+++ Assets/02.Scripts/Drag.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour, IDragHandler
+public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
     private Transform tr;
+    private Transform inventoryTr;
 
     void Start()
     {
         tr = GetComponent<Transform>();        
+        inventoryTr = GameObject.Find("Inventory").GetComponent<Transform>();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         tr.position = Input.mousePosition;
+    }
+
+    public void OnBeginDrag(PointerEventData data)
+    {
+        tr.SetParent(inventoryTr);
     }
 
 }

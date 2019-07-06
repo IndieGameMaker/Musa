7b810e87ce1d281d82440375cd7a21af2083b9ad
IndieGameMaker <myleje@gmail.com>
2019-07-06 13:41:32 +0900

    .

---
+++ Assets/02.Scripts/EnemyCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyCtrl : MonoBehaviour
+{
+    private Transform tr;
+    private Transform playerTr;
+    private NavMeshAgent nv;
+    private Animator anim;
+
+    void Start()
+    {
+        tr          = GetComponent<Transform>();
+        playerTr    = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        nv          = GetComponent<NavMeshAgent>();
+        anim        = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

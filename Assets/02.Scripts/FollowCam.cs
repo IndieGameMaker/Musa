cb02845d74ead9fbba5a2892ae2a40cbe5ce9803
IndieGameMaker <myleje@gmail.com>
2019-07-06 10:50:12 +0900

    .

---
+++ Assets/02.Scripts/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 5.0f;   //타겟과의 거리
+    public float height   = 5.0f;   //타겟과의 높이
+    public float damping  = 3.0f;   //추적할때의 민감도
+
+    private Transform tr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 movePoint = target.position - tr.position;
+        Vector3 move = Vector3.Lerp(tr.position, movePoint, Time.deltaTime * damping);
+
+        tr.position = move + Vector3.right * distance + Vector3.up * height;        
+    }
+}
